0654104309c65d0b102fc70d26e18bdd9c19db58
dhiabsl <dhia.bassalah@gmail.com>
2020-09-23 00:34:46 +0100

    Making a couple of changes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+window.onscroll = function() {myFunctionTwo()};
+
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -6,3 +8,14 @@ function myFunction() {
       x.className = "topnav";
     }
 } 
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunctionTwo() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
